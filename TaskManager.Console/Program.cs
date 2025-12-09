80fa7be22542c2bbf9bfddc9c0044d4fcaf5eef1
Leonardo Ribeiro <Leonardo Ribeiro@LEO>
2025-12-08 22:39:00 -0300

    Dia 2: Criar entidade TaskItem no Core.​

--- TaskManager.Console/Program.cs
+++ TaskManager.Console/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Task Manager Iniciado");
+﻿using TaskManager.Core.Entities;
+
+Console.WriteLine("Task Manager iniciado!");
+
+var task = new TaskItem(1, "Estudar C#", "Ler documentação sobre construtores");
+
+Console.WriteLine($"Id: {task.Id}");
+Console.WriteLine($"Título: {task.Title}");
+Console.WriteLine($"Descrição: {task.Description}");
+Console.WriteLine($"Concluída: {task.IsCompleted}");
+Console.WriteLine($"Criada em (UTC): {task.CreatedAt}");
---
+++ TaskManager.Core/Entities/TaskItem.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace TaskManager.Core.Entities
+{
+    public class TaskItem
+    {
+        public int Id { get; private set; }
+        public string? Title { get; private set; }
+        public string? Description { get; private set; }
+        public bool IsCompleted { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public DateTime? CompletedAt { get; private set; }
+
+        public TaskItem(int id, string title, string? description)
+        {
+            if (string.IsNullOrWhiteSpace(title))
+                throw new ArgumentNullException("Titulo não pode estar vazio", nameof(title));
+            Id = id;
+            Title = title;
+            Description = description;
+            IsCompleted = false;
+            CreatedAt = DateTime.Now;
+            CompletedAt = null;
+        }
+        public void Complete()
+        {
+            if (!IsCompleted)
+                return;
+            IsCompleted=true;
+            CompletedAt = DateTime.Now;
+        }
+    }
+    
+}
